16ea30d3e74b497b0509902881bcee7f312d8458
Juliara-1 <svetlana.me2025@mail.ru>
2025-02-01 00:38:13 +0500

    all files

---
+++ project_2-3.py
@@ -0,0 +1,20 @@
+print("I'm", 'the', "BAD", 'guy')
+print('Как тебя зовут?') 
+print('Привет,', input()) 
+city = "Тула"
+print(city, '- мой город!')
+name1 = 'Тимур'
+name2 = name1
+name1 = 'Гвидо'
+
+print(name1)
+print(name2)
+name_1 = input() 
+print('Привет,', name_1, '!') 
+
+name_2 = input() 
+print('Здравствуйте,', name_2, '.')
+print('aa', 'bb', 'cc', sep="-")
+
+print('БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ', '!', sep='_')
+print('БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ!')
