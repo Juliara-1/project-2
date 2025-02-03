83cefd2d88e9bcdc69907a210fa645e35879d765
Juliara-1 <svetlana.me2025@mail.ru>
2025-02-03 21:14:14 +0500

    all files

--- project2_6.py
+++ project2_6.py
@@ -68,3 +68,65 @@ else:
 
 p = (a + b) * (a + b)
 print(p)
+
+
+
+x=int(input())
+if -1<=x<=17:
+    print('Принадлежит')
+else:
+    print('Не принадлежит')
+
+
+x=int(input())
+if -3<x<7:
+    print('Не принадлежит')
+else:
+    print('Принадлежит')
+
+
+
+n=int(input())
+if 999<n<9999 and ((n%17==0) or (n%7==0)):
+    print('YES')
+else:
+    print('NO')
+
+
+
+a, b, c=int(input()), int(input()), int(input())
+if (a+b>c) and (a+c>b) and (b+c>a):
+    print('YES')
+else:
+    print('NO')
+
+
+n=int(input())
+if n%4==0 and n%100>=1 or n%400==0:  
+    print('YES')
+else:
+    print('NO')
+
+
+x1, y1, x2, y2=int(input()), int(input()), int(input()), int(input())
+if x1==x2 or y1==y2:
+    print('YES')
+else:
+    print('NO')
+
+x1, y1, x2, y2=int(input()), int(input()), int(input()), int(input())
+if (x1+1==x2 or x1-1==x2) and (y1+1==y2 or y1-1==y2):
+    print('YES')
+else:
+    print('NO')
+
+
+
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+if (-1 <= a - c <= 1) and (-1 <= b - d <= 1):
+    print('YES')
+else:
+    print('NO')
+
+
+
