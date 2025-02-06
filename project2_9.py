489fc7043635a673f93bf0b421a05428ebb0c7e7
Juliara-1 <svetlana.me2025@mail.ru>
2025-02-06 14:43:19 +0500

    all files

---
+++ project2_9.py
@@ -0,0 +1,25 @@
+str1 = '1'
+str2 = str1 + '2' + str1
+str3 = str2 + '3' + str2
+str4 = str3 + '4' + str3
+print(str4)
+
+a, b, c=len(input()), len(input()), len(input())
+if (2*b-c-a)*(2*c-b-a)*(2*a-b-c) == 0:
+    print('YES')
+else:
+    print('NO')
+
+
+    if len(s) == 1 and s in 'aeiou':
+    print('YES')
+
+
+
+    n=input()
+if 'суббота' in n:
+    print('YES')
+elif 'воскресенье' in n:
+    print('YES')
+else:
+    print('NO')
